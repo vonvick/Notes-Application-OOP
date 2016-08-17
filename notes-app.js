b51fdb0773c051e4c6277fccda3b15f992fa4a98
johnez1987 <vonvikky@gmail.com>
2016-08-17 15:34:36 +0100

    add more comments and styling to code

--- notes-app.js
+++ notes-app.js
@@ -1,15 +1,25 @@
-// create the class for the note Application
+/**
+ * Class Note that creates the note.
+ * @param {string} author the author name of the note.
+ * @param {string} note_content The note to be stored in the list of notes.
+ * @constructor
+ */
 class Note {
   // constructor for Note Class
   constructor (author, note_content) {
     this.author = author;
     this.note = note_content;
   }
-
 }
 
-class NotesApplication {
-  //constructor for Note Class
+/**
+ * Class NoteApplication that creates the note application.
+ * @param {string} author the author name of the note.
+ * @extends {Note}
+ * @constructor
+ */
+ class NotesApplication {
+  //constructor for NoteApplication Class
   constructor (author) {
     this.name = author;
     this.notes = [];
